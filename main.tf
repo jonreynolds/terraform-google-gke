d6e59c9f918c100f34ef1a0194b295ee6ec5fb6a
Jonathan Reynolds <jonathan@listeningfields.com>
2021-09-27 13:38:02 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -37,33 +37,13 @@ terraform {
 provider "google" {
   project = var.project
   region  = var.region
-
-  scopes = [
-    # Default scopes
-    "https://www.googleapis.com/auth/compute",
-    "https://www.googleapis.com/auth/cloud-platform",
-    "https://www.googleapis.com/auth/ndev.clouddns.readwrite",
-    "https://www.googleapis.com/auth/devstorage.full_control",
-
-    # Required for google_client_openid_userinfo
-    "https://www.googleapis.com/auth/userinfo.email",
-  ]
+  credentials = file(var.gcp_auth_file)
 }
 
 provider "google-beta" {
   project = var.project
   region  = var.region
-
-  scopes = [
-    # Default scopes
-    "https://www.googleapis.com/auth/compute",
-    "https://www.googleapis.com/auth/cloud-platform",
-    "https://www.googleapis.com/auth/ndev.clouddns.readwrite",
-    "https://www.googleapis.com/auth/devstorage.full_control",
-
-    # Required for google_client_openid_userinfo
-    "https://www.googleapis.com/auth/userinfo.email",
-  ]
+  credentials = file(var.gcp_auth_file)
 }
 
 # We use this data provider to expose an access token for communicating with the GKE cluster.
