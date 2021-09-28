d069cbed55afcf5e1b8e09d68e941c7d5ab28e65
Jonathan Reynolds <jonathan@listeningfields.com>
2021-09-28 17:40:50 +0100

    Update main.tf

--- examples/gke-private-cluster/main.tf
+++ examples/gke-private-cluster/main.tf
@@ -28,11 +28,13 @@ terraform {
 provider "google" {
   project = var.project
   region  = var.region
+  credentials = file(var.gcp_auth_file)
 }
 
 provider "google-beta" {
   project = var.project
   region  = var.region
+  credentials = file(var.gcp_auth_file)
 }
 
 # ---------------------------------------------------------------------------------------------------------------------
@@ -148,20 +150,6 @@ resource "google_container_node_pool" "node_pool" {
   }
 }
 
-# ---------------------------------------------------------------------------------------------------------------------
-# CREATE A CUSTOM SERVICE ACCOUNT TO USE WITH THE GKE CLUSTER
-# ---------------------------------------------------------------------------------------------------------------------
-
-module "gke_service_account" {
-  # When using these modules in your own templates, you will need to use a Git URL with a ref attribute that pins you
-  # to a specific version of the modules, such as the following example:
-  # source = "github.com/gruntwork-io/terraform-google-gke.git//modules/gke-service-account?ref=v0.2.0"
-  source = "../../modules/gke-service-account"
-
-  name        = var.cluster_service_account_name
-  project     = var.project
-  description = var.cluster_service_account_description
-}
 
 # ---------------------------------------------------------------------------------------------------------------------
 # CREATE A NETWORK TO DEPLOY THE CLUSTER TO
